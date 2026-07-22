b0265fab176fec681770083a689922c9b509770b
Roy <Rakesh@Apples-MacBook-Pro.local>
2026-07-22 12:30:16 +0530

    Fix Terraform provider consistency and Azure NSG deployment dependency

--- infrastructure/modules/compute/versions.tf
+++ infrastructure/modules/compute/versions.tf
@@ -1,15 +1,7 @@
 terraform {
-
   required_providers {
-
     azurerm = {
-
       source = "hashicorp/azurerm"
-
-      version = "~> 4.80"
-
     }
-
   }
-
 }
--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -22,6 +22,10 @@ resource "azurerm_subnet_network_security_group_association" "web" {
   subnet_id = var.web_subnet_id
 
   network_security_group_id = azurerm_network_security_group.web.id
+
+  depends_on = [
+    azurerm_network_security_group.web
+  ]
 }
 
 
@@ -30,4 +34,8 @@ resource "azurerm_subnet_network_security_group_association" "backend" {
   subnet_id = var.backend_subnet_id
 
   network_security_group_id = azurerm_network_security_group.backend.id
+
+  depends_on = [
+    azurerm_network_security_group.backend
+  ]
 }
