a94a36abfc92a6133aa8d6599d0b80859476d21e
Roy <Rakesh@Apples-MacBook-Pro.local>
2026-07-28 00:37:38 +0530

    added Natgateway to main module

--- infrastructure/modules/natgateway/main.tf
+++ infrastructure/modules/natgateway/main.tf
@@ -9,3 +9,20 @@ resource "azurerm_public_ip" "nat" {
 
   tags = var.tags
 }
+
+resource "azurerm_nat_gateway" "main" {
+
+  name                = "ado-dev-nat-gateway"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku_name = "Standard"
+
+  tags = var.tags
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "main" {
+
+  nat_gateway_id       = azurerm_nat_gateway.main.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
