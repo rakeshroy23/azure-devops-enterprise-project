6608e5db5518aa53418f998eb38af0e1ef0294ad
Roy <Rakesh@Apples-MacBook-Pro.local>
2026-07-28 00:47:21 +0530

    Add Azure NAT Gateway and subnet association

--- infrastructure/modules/natgateway/main.tf
+++ infrastructure/modules/natgateway/main.tf
@@ -26,3 +26,9 @@ resource "azurerm_nat_gateway_public_ip_association" "main" {
   nat_gateway_id       = azurerm_nat_gateway.main.id
   public_ip_address_id = azurerm_public_ip.nat.id
 }
+
+resource "azurerm_subnet_nat_gateway_association" "web" {
+
+  subnet_id      = var.web_subnet_id
+  nat_gateway_id = azurerm_nat_gateway.main.id
+}
