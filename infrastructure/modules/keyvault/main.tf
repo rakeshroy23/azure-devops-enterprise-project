7c337ac7509cdf14d574df8de9d2b0b6288daa09
Roy <Rakesh@Apples-MacBook-Pro.local>
2026-07-28 10:14:34 +0530

    added Azure keyvault for secret management

---
+++ infrastructure/modules/keyvault/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_key_vault" "main" {
+
+  name                = "adodevkv${random_string.suffix.result}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tenant_id = var.tenant_id
+  sku_name  = "standard"
+
+  purge_protection_enabled   = false
+  soft_delete_retention_days = 7
+
+  tags = var.tags
+}
+
+resource "random_string" "suffix" {
+
+  length  = 6
+  upper   = false
+  special = false
+}
